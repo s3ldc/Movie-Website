3959882bfd93f35d7ac2449e6b32a7d16515f14e
Biriy <biriyasunil25@gmail.com>
2026-01-07 15:39:03 +0530

    integrate MovieCard with mock movie data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
+import MovieCard from "./components/MovieCard";
 
 function App() {
   return (
     <>
-    
+      <MovieCard movie={{title: "Avengers: End Game", release_date: "2019"}}/>
     </>
   );
 }
 
 export default App;
+  
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,25 +1,25 @@
+function MovieCard({ movie }) {
+  function onFavouriteClick() {
+    alert("Clicked");
+  }
 
-
-
-function MovieCard({movie}){
-
-    function onFavouriteClick() {
-        alert("Clicked");
-    }
-
-    return <div className="movie-card">
-        <div className="movie-poster">
-            <img src={movie.url} alt={movie.title} />
-            <div className="movie-overlay">
-                <button className="favourite-btn" onClick={onFavouriteClick}>
-                    ♥
-                </button>
-            </div>
+  return (
+    <div className="movie-card">
+      <div className="movie-poster">
+        <img src={movie.url} alt={movie.title} />
+        <div className="movie-overlay">
+          <button className="favourite-btn" onClick={onFavouriteClick}>
+            ♥
+          </button>
         </div>
+      </div>
 
-        <div className="movie-info">
-            <h3>{movie.title}</h3>
-            <p>{movie.release_date}</p>
-        </div>
+      <div className="movie-info">
+        <h3>{movie.title}</h3>
+        <p>{movie.release_date}</p>
+      </div>
     </div>
+  );
 }
+
+export default MovieCard;
