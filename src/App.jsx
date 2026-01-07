5a0db2f03871a76eb4c4bbb7d68bb8681ff86d55
Biriy <biriyasunil25@gmail.com>
2026-01-07 15:32:25 +0530

    add MovieCard component with favourite button

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,8 @@ import "./App.css";
 function App() {
   return (
     <>
-      <Text display="Hello World"/>
-      <Text display="Demo text"/>
+    
     </>
-  );
-}
-
-function Text({display}) {
-  return (
-    <div>
-      <p>{display}</p>
-    </div>
   );
 }
 
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,20 @@
+
+
+
+function MovieCard({movie}){
+
+    function onFavouriteClick() {
+        alert("Clicked");
+    }
+
+    return <div className="movie-card">
+        <div className="movie-poster">
+            <img src={movie.url} alt={movie.title} />
+            <div className="movie-overlay">
+                <button className="favourite-btn" onClick={onFavouriteClick}>
+                    ♥
+                </button>
+            </div>
+        </div>
+    </div>
+}
