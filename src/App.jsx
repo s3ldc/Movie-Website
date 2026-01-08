6641a5f450fd8988e17f5f8e755850bea21ec3f4
Biriy <biriyasunil25@gmail.com>
2026-01-08 16:13:47 +0530

    add movie search bar above movies grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
+import Home from "./pages/Home.jsx";
 
 
 function App() {
 
   return (
     <>
-      
+      <Home/>
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,34 @@
 import MovieCard from "../components/MovieCard";
 
 function Home() {
+  const movies = [
+    { id: 1, title: "Inception", release_date: "2010" },
+    { id: 2, title: "The Matrix", release_date: "1999" },
+    { id: 3, title: "Interstellar", release_date: "2014" },
+    { id: 4, title: "The Dark Knight", release_date: "2008" },
+    { id: 5, title: "Pulp Fiction", release_date: "1994" },
+  ];
 
-    const movies = [
-        {id: 1, title: "Inception", release_date: "2010"},
-        {id: 2, title: "The Matrix", release_date: "1999"},
-        {id: 3, title: "Interstellar", release_date: "2014"},
-        {id: 4, title: "The Dark Knight", release_date: "2008"},
-        {id: 5, title: "Pulp Fiction", release_date: "1994"},
-    ]
+  const handleSearch = () => {};
 
-    return <div className="home">
-        <div className="movies-grid">
-            {movies.map(movie => <MovieCard movie={movie} key={movie.id}/>)}
-        </div>
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-from">
+        <input
+          type="text"
+          placeholder="Search for Movies..."
+          className="search-input"
+        />
+        <button type="submit" className="search-btn">Search</button>
+      </form>
+
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
     </div>
+  );
 }
 
 export default Home;
