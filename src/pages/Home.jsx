902340c5eb89ea55827a295da63879a96683efd4
Biriy <biriyasunil25@gmail.com>
2026-01-09 15:19:02 +0530

    add controlled search input using useState

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,10 @@
 import MovieCard from "../components/MovieCard";
+import { useState } from "react";
 
 function Home() {
+
+  const [searchQuery, setSearchQuery] = useState("");
+
   const movies = [
     { id: 1, title: "Inception", release_date: "2010" },
     { id: 2, title: "The Matrix", release_date: "1999" },
@@ -18,6 +22,8 @@ function Home() {
           type="text"
           placeholder="Search for Movies..."
           className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
         <button type="submit" className="search-btn">Search</button>
       </form>
