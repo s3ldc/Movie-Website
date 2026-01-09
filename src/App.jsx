02c5ee1c812354e23942dce8d9fb75263cbc70dd
Biriy <biriyasunil25@gmail.com>
2026-01-09 15:38:48 +0530

    extend app routing with Favorite page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Home from "./pages/Home.jsx";
+import Favorite from "./pages/Favorite.jsx";
 import { Routes, Route } from "react-router-dom";
 
 
@@ -9,6 +10,7 @@ function App() {
     <main className="main-content">
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/favorite" element={<Favorite/>}/>
       </Routes>
     </main>
   );
