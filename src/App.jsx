8ed51dfd8af03d8eed4c77358ab564a7e212c1b6
Biriy <biriyasunil25@gmail.com>
2026-01-09 15:46:19 +0530

    integrate NavBar with routed pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,21 @@ import "./App.css";
 import Home from "./pages/Home.jsx";
 import Favorite from "./pages/Favorite.jsx";
 import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar.jsx";
 
 
 function App() {
 
   return (
+    <div>
+      <NavBar/>
     <main className="main-content">
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/favorite" element={<Favorite/>}/>
       </Routes>
     </main>
+    </div>
   );
 }
 
