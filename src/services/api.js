bb818b78f22de87e7309c1c0d72d3adcdc19cf1e
Biriy <biriyasunil25@gmail.com>
2026-01-10 17:05:33 +0530

    add API service to search movies by query

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_KEY = "4a9069f28969560118a766721ccb1a22";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
@@ -7,3 +7,8 @@ export const getPopularMovies = async () => {
     return data.results
 };
 
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json();
+    return data.results
+};
