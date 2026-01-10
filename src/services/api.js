220d04ce824450b0fd1adb281baa458d47a0ccc1
Biriy <biriyasunil25@gmail.com>
2026-01-10 17:01:46 +0530

    add API service to fetch popular movies

---
+++ src/services/api.js
@@ -0,0 +1,9 @@
+const API_KEY = "4a9069f28969560118a766721ccb1a22";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results
+};
+
