297915b3edd833d086d230c48d3372dbaca6c38a
Biriy <biriyasunil25@gmail.com>
2026-01-11 17:03:08 +0530

    implement add, remove, and check favorites logic

--- src/contexts/MovieContext.jsx
+++ src/contexts/MovieContext.jsx
@@ -18,6 +18,18 @@ export const MovieProvider = ({children}) => {
         localStorage.setItem("favorites", JSON.stringify(favorites));
     }, [favorites]);
 
+    const addToFavorites = (movie) => {
+        setFavorites(prev => [...prev, movie]);
+    }
+
+    const removeFromFavorites = (movieId) => {
+        setFavorites(prev => prev.filter(movie => movie.id !== movieId));
+    }
+
+    const isFavorites = (movieId) => {
+        return favorites.some(movie => movie.id === movieId);
+    }
+
     return <MovieContext.Provider>
         {children}
     </MovieContext.Provider>
