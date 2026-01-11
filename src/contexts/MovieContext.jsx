956835065d41ee792da68e6d859fd750a295f6c1
Biriy <biriyasunil25@gmail.com>
2026-01-11 16:52:57 +0530

    initialize favorites from localStorage in MovieContext

--- src/contexts/MovieContext.jsx
+++ src/contexts/MovieContext.jsx
@@ -4,6 +4,15 @@ const MovieContext = createContext();
 
 export const useMovieContext = () => useContext(MovieContext);
 
-export const MovieProvider = () => {
-    
+export const MovieProvider = ({children}) => {
+
+    const [favorites, setFavorites] = useState([]);
+
+    useEffect(() => {
+        const storedFavorites = localStorage.getItem("favorites");
+    })
+
+    return <MovieContext.Provider>
+        {children}
+    </MovieContext.Provider>
 }
